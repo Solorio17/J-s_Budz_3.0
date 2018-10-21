744aae497638ee28697e810df716e3e546d0e36d
Jason Solorio <jasonsoloriom@gmail.com>
2018-10-21 01:15:21 -0600

    Route, Router import for navigation. And creating one route.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
  import React, {Component} from 'react';
  import Home from './components/home';
  import ReactDOM from 'react-dom';
+ import { BrowserRouter as Router} from 'react-router-dom';
+ import Route from 'react-router-dom/Route';
+
 
  class App extends Component {
    render(){
      return (
-       <div>
-        <Home />
-       </div>
+       <Router>
+           <div>
+             <Route path="/" exact render={
+                 ()=>{
+                   return (<Home /> );
+                 }
+               }/>
+           </div>
+     </Router>
      );
    }
  }
