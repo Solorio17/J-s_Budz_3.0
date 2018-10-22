2bae6fdefffa702c65070f5d59f76199c3fdf935
Jason Solorio <jasonsoloriom@gmail.com>
2018-10-22 13:43:44 -0600

    Importing the new Routes that will make up the other pages of J's Budz. And creating new routes for each of them --> Budz, Dabz, Ediblez, Drinkz.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
  import React, {Component} from 'react';
- import Home from './components/home';
  import ReactDOM from 'react-dom';
  import { BrowserRouter as Router} from 'react-router-dom';
  import Route from 'react-router-dom/Route';
 
+ import Home from './components/home';
+ import Budz from './components/budz';
+ import Dabz from './components/dabz';
+ import Ediblez from './components/ediblez';
+ import Drinkz from './components/drinkz';
 
  class App extends Component {
    render(){
@@ -15,9 +19,30 @@
                    return (<Home /> );
                  }
                }/>
+             <Route path="/budz" exact render={
+                 ()=>{
+                   return (<Budz /> );
+                 }
+               }/>
+             <Route path="/dabz" exact render={
+                 ()=>{
+                   return (<Dabz /> );
+                 }
+               }/>
+               <Route path="/ediblez" exact render={
+                   ()=>{
+                     return (<Ediblez /> );
+                   }
+                 }/>
+             <Route path="/drinkz" exact render={
+                 ()=>{
+                   return (<Drinkz /> );
+                 }
+               }/>
            </div>
      </Router>
      );
    }
  }
+ 
  ReactDOM.render(<App />, document.querySelector('.app'));
